bca1a4357f870686b6acb04d511f9ec319fd0fe5
brycefish <bryce.k.fish@gmail.com>
2023-08-30 14:50:26 -0600

    changes for vercel deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 "use client"
 import './globals.css'
 import Navbar from '../../components/Navbar';
-import { useState } from 'react';
 import Footer from '../../components/Footer';
 
 export const metadata = {
